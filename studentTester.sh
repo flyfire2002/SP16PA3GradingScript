af28faebf2a00665cfbcb9fdfef70d890a7e473f
Huayin Zhou <cs100s28@ieng6-202.ucsd.edu>
2016-05-15 16:43:57 -0700

    Timeout guard

--- studentTester.sh
+++ studentTester.sh
@@ -2,6 +2,7 @@
 refFolder=~/PA3_Grading/PA3_Final_Grading_ref # Change this var to w/e place you put the reference testers
 gradeFile=./grades.txt
 valgrindOutput=./valgrindOutput.txt
+timeoutVal=240
 
 echo -e "Sanity check - Ref folder: $refFolder\n"
 
@@ -23,19 +24,19 @@ echo -e "Compiling student_fin_tester\n"
 g++ -g -gdwarf-2 -std=c++11 util.cpp DictionaryBST.cpp DictionaryHashtable.cpp DictionaryTrie.cpp student_fin_tester.cpp -o student_fin_tester
 
 echo -e "Test 1: 26 single-char prefixes with runtime\n"
-./student_fin_tester $refFolder/shuffled_freq2.txt out_student_0.txt 10 0 
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_freq2.txt out_student_0.txt 10 0 
 
 echo -e "Test 2: 5 multi-char prefixes with runtime\n"
-./student_fin_tester $refFolder/shuffled_freq2.txt out_student_1.txt 10 1
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_freq2.txt out_student_1.txt 10 1
 
 echo -e "Test 3: 5 multi-chra prefixes with 10 predictComp\n" 
-./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 10 4 
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 10 4 
 
 echo -e "Test 4: 5 multi-chra prefixes with 20 predictComp\n"
-./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 20 4 
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 20 4 
 
 echo -e "Test 5: 5 multi-chra prefixes with 50 predictComp\n"
-./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 50 4
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt out_student_4.txt 50 4
 
 echo -e "Test 6: Valgrind\n"
 valgrind --leak-check=full ./student_fin_tester $refFolder/shuffled_freq_2.txt temp.txt 1 6 > temp.txt 2> $valgrindOutput
@@ -46,10 +47,10 @@ else
 fi
 
 echo -e "Test 7: Empty string\n"
-./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt $gradeFile 10 6 > temp.txt
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt $gradeFile 10 6 > temp.txt
 
 echo -e "Test 8: Fake string\n"
-./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt $gradeFile 10 7 > temp.txt
+timeout $timeoutVal ./student_fin_tester $refFolder/shuffled_unique_freq_dict.txt $gradeFile 10 7 > temp.txt
 
 echo -e "Calc Grade\n"
 $refFolder/fin_tester ./out_student_0.txt ./out_ref_0.txt $gradeFile 2  2> temp.txt
